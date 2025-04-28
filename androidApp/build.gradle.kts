b929f24b6d4d70a296643636dc50e09ba85d4995
Guilherme <guicarneiro.dev@gmail.com>
2025-04-28 02:26:00 -0300

    new app icon

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.guicarneirodev.ltascore.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
-        versionName = "1.6.1"
+        versionCode = 9
+        versionName = "1.6.2"
 
         resourceConfigurations.addAll(listOf("en", "pt"))
     }
