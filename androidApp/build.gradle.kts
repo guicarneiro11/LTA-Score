9e061eb29bbaa999065042c744050c1220a346ca
Guilherme <guicarneiro.dev@gmail.com>
2025-05-06 23:53:49 -0300

    notifications fix

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.guicarneirodev.ltascore.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 14
-        versionName = "1.7.3"
+        versionCode = 15
+        versionName = "1.7.4"
 
         resourceConfigurations.addAll(listOf("en", "pt"))
     }
