a944532216d4559ceb3254a637312b8f35bd81db
Guilherme <guicarneiro.dev@gmail.com>
2025-05-13 16:31:06 -0300

    admin mode implementation

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.guicarneirodev.ltascore.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 16
-        versionName = "1.7.5"
+        versionCode = 17
+        versionName = "1.8.0"
 
         resourceConfigurations.addAll(listOf("en", "pt"))
     }
